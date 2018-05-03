03a554fb89ba783d37166664b3c3e2738ccf87eb
AD <ad.kru13@gmail.com>
2018-05-03 13:52:47 +0300

    fake json server implemented

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -1,6 +1,6 @@
 import { HttpHeaders } from '@angular/common/http';
 
-export const MOCK_URL = './assets/printers.json';
+export const MOCK_URL = 'http://localhost:3000/printers';
 export const HTTP_OPTIONS = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
 };
