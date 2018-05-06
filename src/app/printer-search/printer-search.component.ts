b9e88fa4ea9238ee1965ceac5ebfd9435e03901c
AD <ad.kru13@gmail.com>
2018-05-06 13:10:54 +0300

    searching initialized after 1 sec on input event

--- src/app/printer-search/printer-search.component.ts
+++ src/app/printer-search/printer-search.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
 
 import { PrinterService } from '../services/printer.service';
 
@@ -8,14 +11,15 @@ import { PrinterService } from '../services/printer.service';
   styleUrls: ['./printer-search.component.scss']
 })
 export class PrinterSearchComponent implements OnInit {
+  firstNameControl = new FormControl();
 
   constructor(private printerService: PrinterService) { }
 
   ngOnInit() {
-  }
-
-  getPrinters(printerName: string) {
-    this.printerService.emitNewPrinterObserver(printerName);
+    this.firstNameControl.valueChanges
+      .debounceTime(1000)
+      .distinctUntilChanged()
+      .subscribe(printerName => this.printerService.emitNewPrinterObserver(printerName))
   }
 
 }
